4c6dd52084985660717fc84175ed18d320480a01
mile1983 <milexic@gmail.com>
2017-12-21 19:18:37 +0100

    drugi komit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-       <title>Welcome</title>
+       <title>About</title>
     </head>
     <body>
-      <h1>“Welcome to Cars Project”</h1>
+      <h1>“About us”</h1>
     </body>
 </html>
