a218c7904dab2a0afa95a264f0844b0a5ffc79ad
mile1983 <milexic@gmail.com>
2017-12-21 18:59:02 +0100

    prvi komit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+       <title>Laravel</title>
+    </head>
+    <body>
+      <h1>“Welcome to Cars Project”</h1>
+    </body>
+</html>
